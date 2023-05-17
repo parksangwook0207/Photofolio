a5fb76c22b798f329bad2d9b4f99ad6790e099fe
parksangwook0207 <124150788+parksangwook0207@users.noreply.github.com>
2023-05-17 19:41:03 +0900

    5.17
    작업중

---
+++ Assets/0.Lobbys/0.Script/BGMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMove : MonoBehaviour
+{
+    [SerializeField] private Transform temMove; // 현재 배경과 이어지는 새로운 배경
+    [SerializeField] private float bgDis; // 배경간의 거리
+    [SerializeField] private float moveSpeed; // 배경이동속도
+    [SerializeField] private Vector3 moveDirection; // 배경이 이동하는 위치
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 배경이동
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        if (transform.position.x <= -bgDis)
+        {
+            transform.position = temMove.position - moveDirection * bgDis;
+        }
+    }
+}
