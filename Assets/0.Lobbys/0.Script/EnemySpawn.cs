e6400d6ad48a4382d244438902ef5bf0607ef1c5
parksangwook0207 <124150788+parksangwook0207@users.noreply.github.com>
2023-05-23 18:13:03 +0900

    5.23
    작업중

---
+++ Assets/0.Lobbys/0.Script/EnemySpawn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField] List<GameObject> enemy = new List<GameObject>();
+    [SerializeField] private Transform parent;
+
+   
+
+
+    float spawnCnt = 0f;
+    float nextspawnCnt = 0f;
+
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        spawnCnt += Time.deltaTime;
+        if (spawnCnt > nextspawnCnt)
+        {
+            spawnCnt = 0f;
+            nextspawnCnt = Random.Range(2, 5);
+            Instantiate(enemy[Random.Range(0, enemy.Count)], parent);
+        }
+
+    }
+
+
+}
